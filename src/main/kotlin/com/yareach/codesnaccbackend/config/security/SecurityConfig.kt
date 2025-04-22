3717d88db79026a021f1ccdd68a9234cc51d9c98
yareach8345 <mine8345@naver.com>
2025-04-22 16:31:02 +0900

    add SecurityConfig file
    비밀번호 암호화를 위해 BCryptPasswordEncoder를 반환하는 빈을 추가했으며 그 과정에서 SecurityConfig를 추가했다.

---
+++ src/main/kotlin/com/yareach/codesnaccbackend/config/security/SecurityConfig.kt
@@ -0,0 +1,14 @@
+package com.yareach.codesnaccbackend.config.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun bCryptPasswordEncoder() = BCryptPasswordEncoder()
+}
