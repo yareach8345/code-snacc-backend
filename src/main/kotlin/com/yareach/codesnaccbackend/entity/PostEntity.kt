6cff52e5d3f7f7d06145d6bb545c8598958e0ecd
yareach8345 <mine8345@naver.com>
2025-05-10 23:55:52 +0900

    add: PostEntity 추가
    개시글 데이터를 저장하기 위한 PostEntity 추가

---
+++ src/main/kotlin/com/yareach/codesnaccbackend/entity/PostEntity.kt
@@ -0,0 +1,65 @@
+package com.yareach.codesnaccbackend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.PrePersist
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "posts")
+class PostEntity (
+    @Id
+    @Column(name = "post_id")
+    var id: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    var user: UserEntity,
+
+    @Column(nullable = false)
+    var title: String,
+
+    @Column(nullable = false)
+    var code: String,
+
+    @Column(name = "lang", nullable = false)
+    var language: String,
+
+    @Column(nullable = false)
+    var content: String,
+
+    @Column(nullable = false)
+    var writtenAt: LocalDateTime? = null,
+
+    var deleted: Boolean = false,
+
+    @ManyToMany
+    @JoinTable(
+        name = "post_tags",
+        joinColumns = [JoinColumn(name = "post_id")],
+        inverseJoinColumns = [JoinColumn(name = "tag")]
+    )
+    var tags: MutableList<TagEntity>,
+
+    @ManyToMany
+    @JoinTable(
+        name = "recommends",
+        joinColumns = [JoinColumn(name = "post_id")],
+        inverseJoinColumns = [JoinColumn(name = "user_id")]
+    )
+    var recommend: MutableList<UserEntity>
+) {
+    @PrePersist
+    fun wittenAtNullCheck() {
+        if (writtenAt == null)
+            writtenAt = LocalDateTime.now()
+    }
+
+    fun getRecommendCount() = recommend.size
+}
