237401f2abb4e77c618b33d67646549c746adbc7
yareach8345 <mine8345@naver.com>
2025-05-14 23:19:47 +0900

    add: PostEntity를 PostInfoResponseDto로 변경하는 확장함수 추가

--- src/main/kotlin/com/yareach/codesnaccbackend/extensions/EntityToDtoExtensions.kt
+++ src/main/kotlin/com/yareach/codesnaccbackend/extensions/EntityToDtoExtensions.kt
@@ -1,9 +1,26 @@
 package com.yareach.codesnaccbackend.extensions
 
 import com.yareach.codesnaccbackend.config.security.CustomUserDetails
+import com.yareach.codesnaccbackend.dto.post.PostInfoResponseDto
 import com.yareach.codesnaccbackend.dto.user.UserInfoDto
+import com.yareach.codesnaccbackend.entity.PostEntity
 import com.yareach.codesnaccbackend.entity.UserEntity
+import com.yareach.codesnaccbackend.exception.RequiredFieldIsNullException
 
 fun UserEntity.toUserInfoDto() = UserInfoDto(id, nickname, role.name, banned, quit, warnCnt, icon)
 
 fun UserEntity.toCustomUserDetails() = CustomUserDetails(this)
+
+fun PostEntity.toDto(userId: String?) =
+    PostInfoResponseDto(
+        id = id ?: throw RequiredFieldIsNullException("id", "PostEntity"),
+        writer = writer.toUserInfoDto(),
+        title = title,
+        code = code,
+        language = language,
+        content = content,
+        writtenAt = writtenAt ?: throw RequiredFieldIsNullException("writtenAt", "PostEntity"),
+        tags = getTagList(),
+        recommendCnt = getRecommendCount(),
+        didIRecommend = userId in recommends.map { user -> user.id }
+    )
