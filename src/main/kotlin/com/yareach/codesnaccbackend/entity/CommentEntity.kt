da85f147f349127eb9cecee85143dc080c66066c
yareach8345 <mine8345@naver.com>
2025-05-16 22:43:24 +0900

    add: CommentEntity 추가

---
+++ src/main/kotlin/com/yareach/codesnaccbackend/entity/CommentEntity.kt
@@ -0,0 +1,32 @@
+package com.yareach.codesnaccbackend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "comments")
+class CommentEntity (
+    @Id
+    @Column(name = "comment_id")
+    var id: Int?,
+
+    var content: String,
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    var post: PostEntity,
+
+    @ManyToOne
+    @JoinColumn(name = "writer")
+    var writer: UserEntity,
+
+    @Column(name = "written_at")
+    var writtenAt: LocalDateTime? = null,
+
+    var deleted: Boolean = false,
+)
